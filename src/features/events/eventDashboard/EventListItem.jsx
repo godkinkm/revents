7a2c013a4feb08ae76a7180507f398bc9355d561
Kevin Godkin <Godkinkm@gmail.com>
2021-01-08 22:11:29 +0100

    End of section 4

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Item, Segment, Icon, List, Button } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
-export default function EventListItem({event}) {
+export default function EventListItem({ event, selectEvent, deleteEvent }) {
   return (
     <Segment.Group>
       <Segment>
@@ -17,21 +17,32 @@ export default function EventListItem({event}) {
         </Item.Group>
       </Segment>
       <Segment>
-          <span>
-              <Icon name='clock' /> {event.date}
-              <Icon name='marker' /> {event.venue}
-          </span>
+        <span>
+          <Icon name='clock' /> {event.date}
+          <Icon name='marker' /> {event.venue}
+        </span>
       </Segment>
       <Segment>
-          <List horizontal>
-            {event.attendees.map(attendee => (
-              <EventListAttendee key={attendee.id} attendee={attendee}/>
-            ))}
-          </List>
+        <List horizontal>
+          {event.attendees.map((attendee) => (
+            <EventListAttendee key={attendee.id} attendee={attendee} />
+          ))}
+        </List>
       </Segment>
       <Segment clearing>
-          <div>{event.description}</div>
-          <Button color='teal' floated='right' content='View' />
+        <div>{event.description}</div>
+        <Button
+          onClick={() => deleteEvent(event.id)}
+          color='red'
+          floated='right'
+          content='Delete'
+        />
+        <Button
+          onClick={() => selectEvent(event)}
+          color='teal'
+          floated='right'
+          content='View'
+        />
       </Segment>
     </Segment.Group>
   );
