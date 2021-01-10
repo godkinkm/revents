46f40addffce83c3aa3eb4a546d7192092740f61
Kevin Godkin <Godkinkm@gmail.com>
2021-01-10 15:19:26 +0100

    End of section 5

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import "../src/app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
 
-
 const rootEl = document.getElementById("root");
 
 function render() {
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootEl
+  );
 }
 
 if (module.hot) {
